cecbaf8bbf2577695a4355cd1d1a2953521dd77c
PdtAgencia <pdtagencia@iMac-de-PdtAgencia.local>
2022-06-03 17:57:28 -0500

    firt commit

--- src/Components/CardHomeDoctors/CardHomeDoctors.jsx
+++ src/Components/CardHomeDoctors/CardHomeDoctors.jsx
@@ -1,8 +1,28 @@
+import { CardActionArea, Typography, CardActions, IconButton, Card, CardMedia } from '@mui/material'
+import { Location, Star1 } from 'iconsax-react'
 import React from 'react'
 
-const CardHomeDoctors = () => {
+import './Assets/styles.css'
+
+const CardHomeDoctors = ({ imgDoctor, nameDoctor, descriptionDoctor, cityDoctor }) => {
   return (
-    <div>CardHomeDoctors</div>
+    <div>
+      <div className='div_cardHomeDotors-img'>
+        <CardMedia component='img' height='92' image={imgDoctor} alt={nameDoctor} className='cardHomeDotors_img' />
+      </div>
+      <Card className='cardHomeDotors'>
+        <CardActionArea className='cardHomeDotors_ActionArea'>
+          <Typography variant='h7' component='p' gutterBottom sx={{fontSize: '16px', fontWeight: 700}}>{nameDoctor}</Typography>
+          <Typography gutterBottom sx={{fontSize: '14px', fontWeight: 300}}>{descriptionDoctor}</Typography>
+          <Typography sx={{fontSize: '14px', fontWeight: 200}}><Location size="20" color="#004274" variant="Bold"/>{cityDoctor}</Typography>
+          <CardActions className='cardHomeDotors_Action'>
+            <IconButton className=''>
+              <Star1 className='star_CardDoctors' size="26" color='#FFB82E'/>
+            </IconButton>
+          </CardActions>
+        </CardActionArea>
+      </Card>
+    </div>
   )
 }
 
--- src/Components/HomeDoctors/HomeDoctors.jsx
+++ src/Components/HomeDoctors/HomeDoctors.jsx
@@ -1,21 +1,46 @@
 import { CardMedia, Grid } from '@mui/material'
 import React from 'react'
+import CardHomeDoctors from '../CardHomeDoctors/CardHomeDoctors'
+
+import DrJuanMarioImg from './Assets/Img/DrJuanMario.png'
+import DraNicoleImg from './Assets/Img/DraNicole.png'
+import DrDanielCorreaImg from './Assets/Img/DrDanielCorrea.png'
 
 import PrincipalImg from './Assets/Img/Principal.png'
 
 import './Assets/styles.css'
 
+const elementsDoctors = [
+  {img: DrJuanMarioImg, name: 'Dr. Juan Mario Escobar', description: 'Cirujano Plástico', city: 'Medellín'},
+  {img: DraNicoleImg, name: 'Dra. Nicole Echeverry Salja', description: 'Cirujana Plástica estética y reconstructiva', city: 'Medellín'},
+  {img: DrDanielCorreaImg, name: 'Dr. Daniel Correa', description: 'Cirujano Plástico y Estético', city: 'Medellín'},
+
+]
+
 const HomeDoctors = () => {
   return (
     <Grid container justifyContent='center'>
-      <Grid className='HomeDoctors'>
-        <h1>Doctores</h1>
-        <Grid xs={12} sm={6} md={3} lg={2}>
-          <div className='div_PrincipalImg'>
-            <CardMedia component='img' image={PrincipalImg} className='HomeDoctors_PrincipalImg'/>
-          </div>
+      <div className='div_HomeDoctors'>
+        <Grid container className='HomeDoctors'>
+          <h1>Doctores</h1>
+          <Grid xs={3}>
+            <div className='div_PrincipalImg'>
+              <CardMedia component='img' image={PrincipalImg} className='HomeDoctors_PrincipalImg'/>
+            </div>
+          </Grid>
+          <Grid container direction='row' alignItems='center' justifyContent='center' xs={9} spacing={4}>
+            {
+              elementsDoctors.map((doctor, index) => {
+                return (
+                  <Grid item key={index} className='grid_CardHomeDoctors'>
+                    <CardHomeDoctors imgDoctor={doctor.img} nameDoctor={doctor.name} descriptionDoctor={doctor.description} cityDoctor={doctor.city} />
+                  </Grid>
+                )
+              })
+            }
+          </Grid>
         </Grid>
-      </Grid>
+      </div>
     </Grid>
   )
 }
