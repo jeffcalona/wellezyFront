bdcf8c9d9e52a9a8da362337db36dc3880eca037
PdtAgencia <pdtagencia@iMac-de-PdtAgencia.local>
2022-06-03 13:53:00 -0500

    add Butto Home Card Tour

--- src/Components/CardHomeTurism/CardHomeTurism.jsx
+++ src/Components/CardHomeTurism/CardHomeTurism.jsx
@@ -1,4 +1,4 @@
-import { Card, CardActionArea, CardActions, CardMedia, IconButton, Typography } from '@mui/material'
+import { Card, CardActionArea, CardActions, CardContent, CardMedia, IconButton, Typography } from '@mui/material'
 import React from 'react'
 
 import { Heart, Star1 } from 'iconsax-react'
@@ -8,15 +8,16 @@ import './Assets/styles.css'
 const CardHomeTurism = ({ cardImage, cardTitle, cardDescription }) => {
   return (
     <Card className='cardHomeTurism'>
+      <CardActionArea className='cardHomeTurism_ActionArea'>
         <div className='cardHomeTurism_media'>
           <CardMedia component='img' className='cardHomeTurismImg' image={cardImage} alt={cardTitle}/>
           <CardActions className='cardHomeTurism_heart'>
-            <IconButton>
+            <IconButton className='cardHomeTurism-heart_IconButton'>
               <Heart size="32" color="white"/>
             </IconButton>
           </CardActions>
         </div>
-        <CardActionArea>
+        <CardContent>
             <Typography variant='h7' gutterBottom className='cardHomeTurism_title' >{cardTitle}</Typography>
             <Typography className='cardHomeTurism_description'>{cardDescription}</Typography>
             <CardActions className='cardHomeTurism_CardAction'>
@@ -33,10 +34,11 @@ const CardHomeTurism = ({ cardImage, cardTitle, cardDescription }) => {
                 <Star1 className='star_CardTour' size="26" color='#FFB82E'/>
               </IconButton>
               <IconButton className='cardHomeTurism_IconButton'>
-                <Star1 className='star_CardTour' size="26" color='white'/>
+                <Star1 className='star_CardTour' size="26" color='#FFB82E'/>
               </IconButton>
             </CardActions>
-        </CardActionArea>
+        </CardContent>
+      </CardActionArea>
     </Card>
   )
 }
